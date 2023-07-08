fef9952ea4594ec66d38aa282fdf85eba28857c3
Matias Villan <matias.villan@tecnotree.com>
2023-07-08 15:12:01 -0300

    algunos ajustes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting="Bienvenidos a la Tienda!" />
+      <ItemListContainer greeting="Bienvenidos a la tienda." />
     </div>
   )
 }
