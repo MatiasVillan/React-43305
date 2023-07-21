fce08b275dcfb43c62386c6323fa94b51e3d8a3e
Matias Villan <matias.villan@tecnotree.com>
2023-07-21 17:06:58 -0300

    cambio a la nueva branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import {BrowserRouter, Routes, Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer greeting="Bienvenidos a la tienda." />
+      <BrowserRouter>
+        <NavBar />
+        
+        <ItemListContainer greeting="Bienvenidos a la tienda." />
+      </BrowserRouter>
     </div>
   )
 }
